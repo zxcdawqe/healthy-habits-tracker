6e56377c129152e0fb15e91d5270d24384817b5b
zxc <yariksem12@gmail.com>
2025-12-23 00:18:41 +0700

    создание привычки

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './app.html',
-  styleUrls: ['./app.css'],
+  styleUrls: ['./app.css']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  habitControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(3)]
+  });
+
+  addHabit(): void {
+    if (this.habitControl.valid) {
+      console.log('Создана привычка:', this.habitControl.value);
+      this.habitControl.reset('');
+    }
+  }
+}
